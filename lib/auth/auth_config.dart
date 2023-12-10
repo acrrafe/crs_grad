f8a2ab5a19987407483fe07e1e44621468e3fe68
Aron Rafe <lnsapollo0@gmail.com>
2023-12-10 13:16:21 +0800

    First Commit

--- lib/auth/auth_config.dart
+++ lib/auth/auth_config.dart
@@ -7,18 +7,16 @@ import 'package:aad_oauth/aad_oauth.dart';
 
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-   final Config config = Config(
-      tenant: "c83f55a7-7fe8-4934-b759-09926430aef0",
-      clientId: "d225b77a-b742-4407-9daf-4bb94aebb1ad",
-      scope: "openid profile offline_access",
-      redirectUri: kIsWeb
-          ? ""
-          : Platform.isIOS
-          ? "msauth.com.example.plmCrsGrad://auth"
-          : "msauth://com.example.plm_crs_grad/CZGP42p0PsfXg%2FeNVlQ9LmedzdA%3D",
-      navigatorKey: navigatorKey,
-      loader: SizedBox()
-    // webUseRedirect: true, // default is false - on web only, forces a redirect flow instead of popup auth
-    // //Optional parameter: Centered CircularProgressIndicator while rendering web page in WebView
-    // loader: const Center(child: CircularProgressIndicator()),
+
+   final Config config = new Config(
+    tenant: "YOUR_TENANT_ID",
+    clientId: "YOUR_CLIENT_ID",
+    scope: "openid profile offline_access",
+    // redirectUri is Optional as a default is calculated based on app type/web location
+    redirectUri: "your redirect url available in azure portal",
+    navigatorKey: navigatorKey,
+    webUseRedirect: true, // default is false - on web only, forces a redirect flow instead of popup auth
+    //Optional parameter: Centered CircularProgressIndicator while rendering web page in WebView
+    loader: Center(child: CircularProgressIndicator()),
+    postLogoutRedirectUri: 'http://your_base_url/logout', //optional
   );
